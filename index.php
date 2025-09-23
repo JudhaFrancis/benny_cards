383f0008074a6ceadefe5bd0f59441c8e4115d4c
judha <judha@itoi.org>
2025-09-23 09:48:57 +0530

    initial comment(prod server code)

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Laravel bootstrap file for public_html root
+ * Points to the real Laravel public folder
+ */
+
+// Change the path to point to laravel/public
+require __DIR__ . '/laravel/public/index.php';
---
+++ laravel/resources/js/bootstrap.js
@@ -0,0 +1,56 @@
+window._ = require('lodash');
+
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {
+    console.error("Bootstrap / jQuery failed to load:", e);
+}
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require('axios');
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * NOTE:
+ * Echo + Pusher have been removed because they are not configured
+ * and were causing "Echo is not defined" errors.
+ * 
+ * If you ever need real-time broadcasting again,
+ * uncomment the section below and configure properly.
+ */
+
+/*
+// import Echo from 'laravel-echo';
+// window.Pusher = require('pusher-js');
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     forceTLS: true,
+//     authEndpoint: baseURL + '/broadcasting/auth',
+//     encrypted: true
+// });
+*/
