bf01c268335b6e3cf8b7b72d43e20bc329617067
judha <judha@itoi.org>
2026-02-10 10:10:49 +0530

    chore: remove deleted files

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Laravel bootstrap file for public_html root
- * Points to the real Laravel public folder
- */
-
-// Change the path to point to laravel/public
-require __DIR__ . '/laravel/public/index.php';
